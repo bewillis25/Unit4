6f3e508c9659e83071b56773147aac590d4578a8
bewillis25 <bewillis25@students.bmchs.org>
2022-04-07 09:38:40 -0500

    Lab 4.03

--- Lab 4.03.py
+++ Lab 4.03.py
@@ -80,7 +80,77 @@ For each of the following problems, you will write a function that will draw the
         *****
     
 '''
-# Write the code for your custom function below:
-def my_function():
-    # replace 'pass' with your code
-    pass
+
+# Makes a line of 7 stars
+def draw_7_line():
+        my_string = ''
+        for i in range(0, 7):
+            my_string += ' *'
+        print(my_string)
+# Makes a 7x7 square
+def draw_7():
+    string = ''
+    for i in range(0,7):
+        string += f'{draw_7_line()} \n'
+
+# Draws a line of stars then below it a line of stripes
+def stars_and_stripes():
+    my_string2 = ''
+    my_string = ''
+    for i in range(0,6):
+        my_string2 += ' *'
+    for i in range(0,6):
+        my_string += ' -'
+    print(my_string2)
+    print(my_string)
+# Repeats the above loop three times
+def three_rows_stars_and_stripes():
+    string = ''
+    for i in range(0,3):
+        string += f'{stars_and_stripes()} \n'
+
+# Makes a triangle with increasing numbers per row
+def increasing_number_triangle(num):
+    for i in range(0,num + 1):
+        for j in range(0,i+1):
+            # Used google to find the end='' function in order to print 1234, etc. without going down a line each iteration
+            print(j+1, end='')
+        print()
+
+# Same as stars_and_stripes() but vertical
+def row_one_vertical_stars_and_stripes():
+    string1 = ''
+    string2 = ''
+    for i in range(0,6):
+        if i%2 == 0:
+            string1 = ' -'
+        else:
+            string2 = ' *'
+    print(3*(string1+string2))
+def n_rows_vertical_stars_and_stripes(num):
+    string = ''
+    for i in range(0,num):
+        string += f'{row_one_vertical_stars_and_stripes()} \n'
+
+# Increasing triangle of asterisks with odd number per row
+def increasing_star_triangle(num):
+    for i in range(0,num):
+            print('*'*(2*i+1))
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
